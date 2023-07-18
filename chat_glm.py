0b1f3a4faacde8086e30ebd0f8549a2af09d7804
nzlm <nzlm@163.com>
2023-07-18 17:26:38 +0800

    commit

--- chat_glm.py
+++ chat_glm.py
@@ -6,10 +6,13 @@ import os
 
 
 class Chat(nn.Module):
-    def __init__(self, model_path="../models/chatglm-6b"):
+    def __init__(self, model_path=r"D:\lqh\work\WorldGPT\models\chatglm2-6b"):
         super(Chat, self).__init__()
         self.tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
-        self.model = AutoModel.from_pretrained(model_path, trust_remote_code=True).half().cuda()
+        if torch.cuda.is_available():
+            self.model = AutoModel.from_pretrained(model_path, trust_remote_code=True).half().cuda()
+        else:
+            self.model = AutoModel.from_pretrained(model_path, trust_remote_code=True).float()
         self.model.eval()
 
     def forward(self, text, history):
@@ -23,8 +26,8 @@ class PtChat(nn.Module):
     def __init__(self, model_path, checkpoint_path, pre_seq_len):
         super(PtChat, self).__init__()
         self.tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
-        config = AutoConfig.from_pretrained("../models/chatglm-6b", trust_remote_code=True, pre_seq_len=pre_seq_len)
-        self.model = AutoModel.from_pretrained("../models/chatglm-6b", config=config, trust_remote_code=True)
+        config = AutoConfig.from_pretrained(model_path, trust_remote_code=True, pre_seq_len=pre_seq_len)
+        self.model = AutoModel.from_pretrained(model_path, config=config, trust_remote_code=True)
         prefix_state_dict = torch.load(os.path.join(checkpoint_path, "pytorch_model.bin"))
         new_prefix_state_dict = {}
         for k, v in prefix_state_dict.items():
@@ -51,10 +54,10 @@ if __name__ == '__main__':
     print(res)
     print(time_)
 
-    glm_path = '../models/chatglm-6b'
-    pt_path = 'output/adgen-chatglm-6b-pt-256-2e-2/checkpoint-3000'
-    predict_max_len = 256
-    pt_chat = PtChat(glm_path, pt_path, predict_max_len)
-    res, time_ = pt_chat(sentence, [])
-    print(res)
-    print(time_)
+    # glm_path = '../models/chatglm-6b'
+    # pt_path = 'output/adgen-chatglm-6b-pt-256-2e-2/checkpoint-3000'
+    # predict_max_len = 256
+    # pt_chat = PtChat(glm_path, pt_path, predict_max_len)
+    # res, time_ = pt_chat(sentence, [])
+    # print(res)
+    # print(time_)
